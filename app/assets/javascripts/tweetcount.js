194aaefbdcc57eadb7f4ff231fa176e328f65dd6
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2015-05-28 09:56:19 +0900

    javascriptの場所を移動

--- app/assets/javascripts/tweetcount.js
+++ app/assets/javascripts/tweetcount.js
@@ -1,7 +1,7 @@
 $(function(){
   var textarea = document.getElementById('tweetarea');
   Rx.Observable.fromEvent(textarea, 'input')
-    .map(function(x) { return x.target.textLength })
+    .map(function(x) { return x.target.textLength; })
     .subscribe(function(length) {
       var tweetcount = document.getElementById('tweetcount');
       tweetcount.innerHTML = length;
